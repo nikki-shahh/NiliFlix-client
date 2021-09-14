e6f384cb89c3137a9b14f2bec01db5152118d4f5
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2021-09-13 20:06:28 -0700

    Adds login and reg views and fetching data from API plus some edits

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import PropTypes from "prop-types";
+
+//import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
-        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+        return (
+            <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+        );
     }
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Release: PropTypes.number,
+        Rating: PropTypes.number,
+        ImagePath: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string,
+            Description: PropTypes.string
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string,
+            Bio: PropTypes.string,
+            Birth: PropTypes.number,
+            Movies: PropTypes.array
+        }),
+        Actors: PropTypes.array.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from "prop-types";
+
+//import "./movie-view.scss";
 
 export class MovieView extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            movie: [],
+            onBackClick: null
+        };
+    }
     render() {
         const { movie, onBackClick } = this.props;
 
@@ -10,24 +20,59 @@ export class MovieView extends React.Component {
                 <div className="movie-poster">
                     <img src={movie.ImagePath} />
                 </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
+                <div className="movie-body">
+                    <div className="movie-title">
+                        <span className="label">Title: </span>
+                        <span className="value">{movie.Title}</span>
+                    </div>
+                    <div className="movie-description">
+                        <span className="label">Description: </span>
+                        <span className="value">{movie.Description}</span>
+                    </div>
+                    <div className="movie-genre">
+                        <span className="label">Genre: </span>
+                        <span className="value">{movie.Genre}</span>
+                    </div>
+                    <div className="movie-director">
+                        <span className="label">Director: </span>
+                        <span className="value">{movie.Director}</span>
+                    </div>
+                    <div className="movie-actors">
+                        <span className="label">Actors: </span>
+                        <span className="value">{movie.Actors}</span>
+                    </div>
+                    <div className="movie-release">
+                        <span className="label">Release: </span>
+                        <span className="value">{movie.release}</span>
+                    </div>
+                    <div className="movie-rating">
+                        <span className="label">Rating: </span>
+                        <span className="value">{movie.Rating}</span>
+                    </div>
+                    <button onClick={() => { onBackClick(null) }}>Back</button>
                 </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </div>
-                <div className="movie-genre">
-                    <span className="label">Genre: </span>
-                    <span className="value">{movie.Genre}</span>
-                </div>
-                <div className="movie-director">
-                    <span className="label">Director: </span>
-                    <span className="value">{movie.Director}</span>
-                </div>
-                <button onClick={() => { onBackClick(null) }}>Back</button>
             </div>
         );
     }
 }
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Release: PropTypes.number,
+        Rating: PropTypes.number,
+        ImagePath: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string,
+            Description: PropTypes.string
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string,
+            Bio: PropTypes.string,
+            Birth: PropTypes.number,
+            Movies: PropTypes.array
+        }),
+        Featured: PropTypes.bool.isRequired,
+        Actors: PropTypes.array.isRequired
+    }),
+};
